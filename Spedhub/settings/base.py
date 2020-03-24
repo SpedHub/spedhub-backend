57bff427033ce26314f0a115c165c1232fd12a35
Arek Karwasz <arekkarwasz@wp.pl>
2020-03-24 20:08:16 +0100

    django-cors-headers has been added, fixed docker-compose for frontend application

--- Spedhub/settings/base.py
+++ Spedhub/settings/base.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'main',
     'rest_framework',
     'knox',
+    'corsheaders',
 ]
 
 REST_FRAMEWORK = {
@@ -35,11 +36,17 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+ 'http://localhost:8081'
 ]
 
 ROOT_URLCONF = 'Spedhub.urls'
